d1a84a703a1e6debbeb6b2d80fb60a93f923269e
Rrezakk <52713544+Rrezakk@users.noreply.github.com>
2023-02-03 18:08:37 +0300

    Bootstrap and index

--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CRUD_The_First.Data;
+using CRUD_The_First.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CRUD_The_First.Controllers;
 
 public class FileController : Controller
 {
+    private readonly ApplicationContext _context;
+    public FileController(ApplicationContext context)
+    {
+        this._context = context;
+    }
     // GET
     public IActionResult Index()
     {
-        return View();
+        IEnumerable<FIleModel> objFileList = _context.Files;
+        return View(objFileList);
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationContext>(options=> options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")),ServiceLifetime.Singleton);
-
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
